1b3dc6727543e79a7a18dc4cc417e65d3f778762
FIXED-TERM Georgiev Stefan (RBBG/ENG16) <ges2sf@bosch.com>
2024-10-10 12:47:46 +0300

    with MVS, docs, parts

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -33,7 +33,7 @@ void loop() {
   const char *message = "Hello from Arduino!\n";
   SerialTinyUSB.write((const uint8_t *)message, strlen(message));
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
+  delay(250);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);
+  delay(250);
 }
